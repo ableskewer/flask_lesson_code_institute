a243cf24ae002406a878013d124809ead9c2f470
ableskewer <danse4@msn.com>
2020-07-10 23:10:15 +0000

    ko

--- run.py
+++ run.py
@@ -35,6 +35,7 @@ def about_member(member_name):
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
+        print(request)
         flash("Thanks {}, we have received your message".format(
             request.form["name"]))
     return render_template("contact.html", page_title="Contact")
